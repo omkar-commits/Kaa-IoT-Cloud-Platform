333db14a374a1aa1101c4d0ba8c85caa0c45674a
Omkar Pabshetwar <omkar.pabshetwar2@mail.dcu.ie>
2020-12-01 23:50:47 +0000

    Update main.py

---
+++ main.py
@@ -0,0 +1,87 @@
+# Simple MQTT-based endpoint metadata management client for the Kaa IoT platform.
+# See https://docs.kaaiot.io/KAA/docs/current/Tutorials/getting-started/connecting-your-first-device/.
+
+import itertools
+import json
+import queue
+import random
+import string
+import sys
+import time
+
+import paho.mqtt.client as mqtt
+
+KPC_HOST = "mqtt.cloud.kaaiot.com"  # Kaa Cloud plain MQTT host
+KPC_PORT = 1883                     # Kaa Cloud plain MQTT port
+
+APPLICATION_VERSION = "bv1prctbhnjfabje538g-v1"  # Paste your application version
+ENDPOINT_TOKEN = "token1"       # Paste your endpoint token
+
+
+class MetadataClient:
+
+    def __init__(self, client):
+        self.client = client
+        self.metadata_by_request_id = {}
+        self.global_request_id = itertools.count()
+        get_metadata_subscribe_topic = f'kp1/{APPLICATION_VERSION}/epmx/{ENDPOINT_TOKEN}/get/#'
+        self.client.message_callback_add(get_metadata_subscribe_topic, self.handle_metadata)
+
+    def handle_metadata(self, client, userdata, message):
+        request_id = int(message.topic.split('/')[-2])
+        if message.topic.split('/')[-1] == 'status' and request_id in self.metadata_by_request_id:
+            print(f'<--- Received metadata response on topic {message.topic}')
+            metadata_queue = self.metadata_by_request_id[request_id]
+            metadata_queue.put_nowait(message.payload)
+        else:
+            print(f'<--- Received bad metadata response on topic {message.topic}:\n{str(message.payload.decode("utf-8"))}')
+
+    def get_metadata(self):
+        request_id = next(self.global_request_id)
+        get_metadata_publish_topic = f'kp1/{APPLICATION_VERSION}/epmx/{ENDPOINT_TOKEN}/get/{request_id}'
+
+        metadata_queue = queue.Queue()
+        self.metadata_by_request_id[request_id] = metadata_queue
+
+        print(f'---> Requesting metadata by topic {get_metadata_publish_topic}')
+        self.client.publish(topic=get_metadata_publish_topic, payload=json.dumps({}))
+        try:
+            metadata = metadata_queue.get(True, 5)
+            del self.metadata_by_request_id[request_id]
+            return str(metadata.decode("utf-8"))
+        except queue.Empty:
+            print('Timed out waiting for metadata response from server')
+            sys.exit()
+
+    def patch_metadata_unconfirmed(self, metadata):
+        partial_metadata_udpate_publish_topic = f'kp1/{APPLICATION_VERSION}/epmx/{ENDPOINT_TOKEN}/update/keys'
+
+        print(f'---> Reporting metadata on topic {partial_metadata_udpate_publish_topic}\nwith payload {metadata}')
+        self.client.publish(topic=partial_metadata_udpate_publish_topic, payload=metadata)
+
+
+def main():
+    # Initiate server connection
+    print(f'Connecting to Kaa server at {KPC_HOST}:{KPC_PORT} using application version {APPLICATION_VERSION} and endpoint token {ENDPOINT_TOKEN}')
+
+    client_id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    client = mqtt.Client(client_id=client_id)
+    client.connect(KPC_HOST, KPC_PORT, 60)
+    client.loop_start()
+
+    metadata_client = MetadataClient(client)
+
+    # Fetch current endpoint metadata attributes
+    retrieved_metadata = metadata_client.get_metadata()
+    print(f'Retrieved metadata from server: {retrieved_metadata}')
+
+    # Do a partial endpoint metadata update
+    metadata_to_report = json.dumps({"model": "BFG 9000", "mac": "00-14-22-01-23-45"})
+    metadata_client.patch_metadata_unconfirmed(metadata_to_report)
+
+    time.sleep(5)
+    client.disconnect()
+
+
+if __name__ == '__main__':
+    main()
